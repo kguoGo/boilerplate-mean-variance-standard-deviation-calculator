085f07a2f5616e0fc4f668ac67218e923fa97d30
kguoGo guo <kathln.yg@gmail.com>
2024-07-08 15:36:42 +0000

    Calculator to github

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,34 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) < 9:
+        print("List must contain nine numbers.")
+    else: 
+        rshp = np.array(list).reshape(3, 3)
+        calculations = {"mean": [], "variance": [], "standard deviation": [], "max": [], 
+                    "min": [], "sum": []}
+        calculations["mean"].append(np.mean(rshp, axis=0).tolist())
+        calculations["mean"].append(np.mean(rshp, axis=1).tolist())
+        calculations["mean"].append(np.mean(rshp)) 
 
+        calculations["variance"].append(np.var(rshp, axis = 0).tolist())
+        calculations["variance"].append(np.var(rshp, axis = 1).tolist()) 
+        calculations["variance"].append(np.var(rshp).tolist())
 
+        calculations["standard deviation"].append(np.std(rshp, axis = 0).tolist())
+        calculations["standard deviation"].append(np.std(rshp, axis = 1).tolist()) 
+        calculations["standard deviation"].append(np.std(rshp).tolist())
 
+        calculations["max"].append(np.amax(rshp, axis = 0).tolist())
+        calculations["max"].append(np.amax(rshp, axis = 1).tolist()) 
+        calculations["max"].append(np.amax(rshp).tolist())
 
-    return calculations
+        calculations["min"].append(np.amin(rshp, axis = 0).tolist())
+        calculations["min"].append(np.amin(rshp, axis = 1).tolist()) 
+        calculations["min"].append(np.amin(rshp).tolist())
+
+        calculations["sum"].append(np.sum(rshp, axis = 0).tolist())
+        calculations["sum"].append(np.sum(rshp, axis = 1).tolist()) 
+        calculations["sum"].append(np.sum(rshp).tolist())
+
+        return calculations
